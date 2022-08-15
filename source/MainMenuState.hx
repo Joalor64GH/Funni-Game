cef256be6d6130f8c527e92ffcbb09a2d27d970f
Joalor64 <rhgarcon@gmail.com>
2022-08-15 14:01:18 -0400

    oopsies

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -19,9 +19,9 @@ class MainMenuState extends FlxState
 		// Create menu
 		Menu.title = "Remind me to come up with a name later";
 		#if !web
-		Menu.options = ['Play', 'Mods', 'Webm Loader', 'Options', 'Exit'];
+		Menu.options = ['Play', 'Mods', 'Options', 'Exit'];
 		#else
-		Menu.options = ['Play', 'Mods', 'Webm Loader', 'Options'];
+		Menu.options = ['Play', 'Mods', 'Options'];
 		#end
 		Menu.includeExitBtn = false;
 		Menu.callback = (option:MenuSelection) ->
@@ -36,11 +36,8 @@ class MainMenuState extends FlxState
 				case 1:
 					trace('Mods');
 				case 2:
-					trace('Webm Loader');
-					FlxG.switchState(new VideoState());
-				case 3:
 					trace('Options');
-				case 4:
+				case 3:
 					trace('Exit');
 					#if (windows || cpp)
 					Sys.exit(0);
