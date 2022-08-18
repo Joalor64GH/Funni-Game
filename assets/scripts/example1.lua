68d2dbb04ecc482e04a90f3430493432d107413b
Joalor64 <rhgarcon@gmail.com>
2022-08-18 14:06:43 -0400

    script examples

---
+++ assets/scripts/example1.lua
@@ -0,0 +1,15 @@
+function onCreate()
+	-- triggered when the lua file is started, some variables weren't created yet
+end
+
+function onCreatePost()
+	-- end of "create"
+end
+
+function onUpdate(elapsed)
+	-- start of "update", some variables weren't updated yet
+end
+
+function onDestroy()
+	-- triggered when the lua file is ended 
+end
